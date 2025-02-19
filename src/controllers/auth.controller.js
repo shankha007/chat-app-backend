df1f2639251f3ee351ddc81656052c87cb9928b8
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-02-20 00:44:50 +0530

    message model and cors setup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const signup = async (req, res) => {
       res.status(400).json({ message: "Invalid user data" });
     }
   } catch (error) {
-    console.log("Error in signup controller", error.message);
+    console.error("Error in signup controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -74,7 +74,7 @@ export const login = async (req, res) => {
       profilePic: user.profilePic,
     });
   } catch (error) {
-    console.log("Error in login controller", error.message);
+    console.error("Error in login controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -85,7 +85,7 @@ export const logout = (req, res) => {
 
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
-    console.log("Error in logout controller", error.message);
+    console.error("Error in logout controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -110,7 +110,7 @@ export const updateProfile = async (req, res) => {
 
     res.status(200).json(updatedUser);
   } catch (error) {
-    console.log("Error in updateProfile controller:", error.message);
+    console.error("Error in updateProfile controller:", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
@@ -119,7 +119,7 @@ export const checkAuth = (req, res) => {
   try {
     res.status(200).json(req.user);
   } catch (error) {
-    console.log("Error in checkAuth controller:", error.message);
+    console.error("Error in checkAuth controller:", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
