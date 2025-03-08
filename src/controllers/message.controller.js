f634482426c67bb065724736f8547a2da4d87e3e
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-03-08 14:27:25 +0530

    implemented real-time chatting with socket

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -57,7 +58,11 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    // TODO: realtime functionality goes here => socket.io
+    // realtime functionality chatting using socket.io
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -24,7 +24,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server is running on PORT " + PORT);
   connectDB();
 });
