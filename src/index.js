86cd08f21457c546c2a2869ac650cf648344c4d6
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-03-08 14:46:22 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -11,6 +12,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
@@ -23,6 +25,16 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../chat-app-frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "./chat-app-frontend", "dist", "index.html")
+    );
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT " + PORT);
